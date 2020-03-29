80070ac9cd5a3e9f9968753c772318ae76315668
MICHAEL <2280514662@qq.com>
2020-03-29 23:12:55 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import WallpaperFooter from '@/components/footer/WallpaperFooter'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   routes: [
     {
       path: '/WallpaperHeader',
